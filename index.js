297a28548580502c11af19d34a65a654b28993e9
Santiago Reynoso <snreynoso@gmail.com>
2020-10-22 16:32:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = new express();
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+// SERVER PORT
+app.listen(PORT, () => {
+    console.log('Web server running on port', PORT);
+});
+
+//  BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
